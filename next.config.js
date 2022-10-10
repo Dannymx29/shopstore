ae4154ce714374df815996944aeaa59e011e09df
DannyMX29 <bran.dan.her.gar@gmail.com>
2022-10-10 14:37:32 -0500

    configuracion inicial he impotacion de proyecto de react para cambio naex

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
+  env: {
+    customKey: 'customValue',
+  },
+  basePath: '/dist',
+  compress: true,
+  async redirects(){
+    return[
+      {
+        source: '/hola',
+        destination: '/hi',
+        permanent: true,
+      }
+    ]
+  }
 }
-
-module.exports = nextConfig
