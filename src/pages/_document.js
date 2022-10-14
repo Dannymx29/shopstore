2cb3c61f375924a814f5922bfb0ca59920ac55c2
DannyMX29 <bran.dan.her.gar@gmail.com>
2022-10-14 12:46:05 -0500

    next/head: SEO en Next.js

---
+++ src/pages/_document.js
@@ -0,0 +1,35 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+
+    render() {
+
+        return (
+
+            <Html lang ='en'>
+
+                <Head>
+
+                    <title>
+                        
+                        React Shop
+
+                    </title>
+
+                </Head>
+
+                <body>
+
+                    <Main />
+
+                    <NextScript />
+
+                </body>
+
+            </Html>
+
+        )
+
+    }
+
+}
